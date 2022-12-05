7d39a3c0145358c356ebdf4b3e41a774c55f34aa
julioznava <junavarro.rojas@gmail.com>
2022-12-05 17:23:10 -0300

    crftoken

--- Recismart/settings.py
+++ Recismart/settings.py
@@ -131,3 +131,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['recismart-pre-production.up.railway.app']
